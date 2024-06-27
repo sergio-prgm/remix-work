4c176c0b73f1bb8189893f6b783f8f496047744b
Sergio Esteve <sergio.esteve@eviden.com>
2024-06-27 20:41:05 +0200

    Feat: render candidates table (WIP)

--- app/routes/_main.candidates._index/route.tsx
+++ app/routes/_main.candidates._index/route.tsx
@@ -1,14 +1,29 @@
-import { Link, Outlet } from "@remix-run/react";
-import { Button } from "~/components/ui/button";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { DataTable } from "~/components/data-table";
+import { BACKEND_URL } from "~/lib/config";
+import { columns } from "./columns";
+
+export async function loader() {
+  const data = await fetch(BACKEND_URL + "/candidate/api/v1/candidates", {
+    method: "GET",
+  });
+  return json({
+    jobOffers: await data.json(),
+  });
+}
 
 export default function Candidates() {
+  const { jobOffers } = useLoaderData<typeof loader>();
+  console.log(jobOffers)
   return (
     <div>
-      <h1>Hello candidates</h1>
-      <Button>
-        <Link to="/candidates/new">New</Link>
-      </Button>
-      <Outlet />
+      <div className="flex justify-between w-full items-center pb-6 sm:px-4">
+        <h1>Hello Job Offers</h1>
+      </div>
+      <div className="sm:px-4">
+        <DataTable columns={columns} data={jobOffers} />
+      </div>
     </div>
   );
 }
